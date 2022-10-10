0e3a1d0237aaba6ef58ec5558f777d29a9a6ef57
Pelayo <pela260@gmail.com>
2022-10-10 10:08:32 +0200

    grupo de musica favorito

--- Prueba.java
+++ Prueba.java
@@ -2,8 +2,8 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Santos Fernández");
 		System.out.println("22 años 1999");
 		System.out.println("Hamburguesa");
+		System.out.println("Extremoduro");
 	}
 }
