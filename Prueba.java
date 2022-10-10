0477f62d1968e7f01ca9c8fb7951f91d3173ba47
Pelayo <pela260@gmail.com>
2022-10-10 08:59:53 +0200

    apellidos, edad, año de nacimiento y comida favorita

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba {
 	public static void main(String args[]) {
 
 		System.out.println("Santos Fernández");
-
+		System.out.println("22 años 1999");
+		System.out.println("Hamburguesa");
 	}
 }
